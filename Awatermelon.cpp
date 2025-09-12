1bb7cba90a2929c7c0a35e673ef1bea87afb516d
Ruchika Lodhi <lodhi03ruchika@gmail.com>
2025-09-12 22:44:32 +0530

    Add files via upload

---
+++ Awatermelon.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    cin >> n;
+    if (((n - 2) % 2 == 0) && (n - 2 > 0))
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+    puts("");
+    return 0;
+}
